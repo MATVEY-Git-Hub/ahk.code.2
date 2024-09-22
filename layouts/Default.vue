4685085d4813657fb38ea17581f46c442bdbba11
MATVEY-Git-Hub <75066032+MATVEY-Git-Hub@users.noreply.github.com>
2024-09-22 20:08:32 +0300

    Update Default.vue

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,32 +1,39 @@
 <template>
-    <div>
+  <div class="flex flex-col min-h-screen">
+    <!-- Основной контент -->
+    <div class="flex-grow">
       <GradientBackground />
       <Topbar />
-        <NuxtPage />
-        <nav class="topbar w-full py-4 fixed top-0 z-50 flex items-center">
-            <div class="flex items-center ml-4">
-                <img src="/public/logo.svg" class="h-8 mobile-logo" />
-            </div>
-        </nav>
-      <div class="absolute bottom-0 right-0 mb-4 mr-4">
+      <NuxtPage />
+      <nav class="topbar w-full py-4 fixed top-0 z-50 flex items-center">
+        <div class="flex items-center ml-4">
+          <img src="/logo.svg" class="h-8 mobile-logo" />
+        </div>
+      </nav>
+    </div>
+
+    <!-- Нижняя часть страницы -->
+    <div class="flex justify-between items-end p-4">
+      <!-- Версия -->
+      <div class="SecVersion bg-slate-800 rounded-xl">
+        V. 2.0.0-β
+      </div>
+
+      <!-- Кнопка GitHub -->
+      <div>
         <Button class="buttonGH font-medium" @click="github">
           <i class="mdi mdi-github"></i> Код на GitHub
         </Button>
       </div>
-      <div class="absolute bottom-0 left-0 mb-4 ml-4">
-        <section class="SecVersion bg-slate-800 rounded-xl">
-          V. 2.0.0-β
-        </section>
-      </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import GradientBackground from '~/components/GradientBackground.vue';
-  import Topbar from '~/components/Topbar.vue';
+  </div>
+</template>
 
-  function github() {
-    window.open('https://github.com/MATVEY-Git-Hub/auto.prikaz.2', '_blank');
-    }
-    </script>
-  
+<script setup lang="ts">
+import GradientBackground from '~/components/GradientBackground.vue';
+import Topbar from '~/components/Topbar.vue';
+
+function github() {
+  window.open('https://github.com/MATVEY-Git-Hub/auto.prikaz.2', '_blank');
+}
+</script>
