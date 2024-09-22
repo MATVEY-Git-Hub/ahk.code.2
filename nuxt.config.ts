658d4d443efb16bcdc12c2fbb37db2c80931a46d
MATVEY-Git-Hub <75066032+MATVEY-Git-Hub@users.noreply.github.com>
2024-09-22 21:55:05 +0300

    Update 2.0.0B realise

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,18 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css', '@mdi/font/css/materialdesignicons.css'],
 
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      title: 'Автоматические приказы',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Здесь можно исправить и создать приказы' },
+        { hid: 'og:title', property: 'og:title', content: 'Автоматические приказы' },
+        { hid: 'og:description', property: 'og:description', content: 'Здесь можно исправить и создать приказы' },
+        { hid: 'og:image', property: 'og:image', content: 'https://auto-prikaz.vercel.app/preview.webp' },
+        { hid: 'og:url', property: 'og:url', content: 'https://auto-prikaz.vercel.app' },
+        { hid: 'og:type', property: 'og:type', content: 'website' }
+      ]
+    }
   },
 
   postcss: {
--- pages/About.vue
+++ pages/About.vue
@@ -1,16 +1,17 @@
-<script lang="ts" setup>
-definePageMeta({
-  layout: 'custom',
-});
-</script>
-
 <template>
-  <head>
-    <title>О нас</title>
-    <meta name="description" content="Страница о нас">
-  </head>
 
-  <div>
-    <h1>About page</h1>
-  </div>
-</template>  
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
+import Topbar from '~/components/Topbar.vue';
+import { useRouter } from 'vue-router';
+
+useSeoMeta({
+title: 'Создание приказов',
+ogTitle: 'Создание приказов',
+description: 'Автоматическое создание приказов',
+ogDescription: 'Автоматическое создание приказов',
+})
+
+</script>
