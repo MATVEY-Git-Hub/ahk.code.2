240699003bcd0ee3e13d10bdb34f5a233f567d79
MATVEY-Git-Hub <75066032+MATVEY-Git-Hub@users.noreply.github.com>
2024-09-22 19:56:58 +0300

    Update Default.vue

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -9,7 +9,7 @@
             </div>
         </nav>
       <!-- Кнопка GitHub -->
-      <div class="absolute bottom-0 right-0 mb-4 mr-4 z-11">
+      <div class="absolute bottom-0 right-0 mb-4 mr-4 z-9">
         <Button class="buttonGH font-medium" @click="github">
           <i class="mdi mdi-github"></i> Код на GitHub
         </Button>
